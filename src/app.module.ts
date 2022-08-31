410cf67c48cd26f0f54af1e7a34c8ab832cb94a1
nam <namgh627@naver.com>
2022-08-31 12:31:41 +0900

    feat/#2/firstsetting setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: 3306,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+    ConfigModule.forRoot({ isGlobal: true }), //
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
