5ada3b4eba47cd02eb6d7046a0c5d908f0ecabc3
jarana <shna231@gmail.com>
2022-09-01 11:00:23 +0900

    feat/#8/user jwt 로그인

--- src/apis/auth/auth.module.ts
+++ src/apis/auth/auth.module.ts
@@ -6,10 +6,24 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), UserModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '100s' },
+      }),
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  // tmp, use tmp UserService
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.userService.findOne(email);
     if (user && user.password === pass) {
@@ -13,5 +16,12 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
