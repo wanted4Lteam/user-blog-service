a351071fd20b32cbbd73dc04b2e6390246bd10e1
Lee <94841127+galaxian@users.noreply.github.com>
2022-09-01 11:35:08 +0900

    test/wanted4Lteam/user-blog-service#12/usertest 회원가입 api 테스트코드 작성

---
+++ src/apis/user/user.service.spec.ts
@@ -0,0 +1,55 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserInput } from 'src/apis/user/dto/user.input';
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
+
+describe('UserService', () => {
+  let userService: UserService;
+
+  const mockUserRepository = {
+    save: jest.fn().mockImplementation((user) =>
+      Promise.resolve({
+        id: Date.now(),
+        ...user,
+      }),
+    ),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
+    }).compile();
+
+    userService = module.get<UserService>(UserService);
+  });
+
+  it('should be defined', () => {
+    expect(userService).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    it('회원가입을 진행 후 user pk 반환.', async () => {
+      const createUser: any = {
+        email: 'abcd@gmail.com',
+        name: '사용자',
+        gender: '남성',
+        age: 20,
+        password: '1234',
+        phone: '010-1234-5678',
+      };
+
+      const result = await userService.create(createUser);
+
+      expect(result).toEqual({
+        id: expect.any(Number),
+      });
+    });
+  });
+});
