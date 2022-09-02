52f5410eff41c32fda7ff2f64f894c47e65464bc
jarana <shna231@gmail.com>
2022-09-02 10:52:49 +0900

    feat/#8/auth login 경로 변경

--- src/apis/auth/auth.controller.ts
+++ src/apis/auth/auth.controller.ts
@@ -8,7 +8,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
+  @Post('users/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
