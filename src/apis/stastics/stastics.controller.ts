4923e53191769a297d81e978b428ebb19324d453
jarana <shna231@gmail.com>
2022-09-05 14:50:18 +0900

    fix/#9/stastics 통계 API Role 추가

--- src/apis/stastics/stastics.controller.ts
+++ src/apis/stastics/stastics.controller.ts
@@ -23,6 +23,7 @@ export class StasticsController {
     summary: '성별에 따른 사용자 조회 API',
     description: '남성, 또는 여성 사용자의 수를 조회합니다.',
   })
+  @Rules('admin')
   async findByGender(@Param('gender') gender: string) {
     return await this.stasticsService.findByGender(gender);
   }
@@ -32,6 +33,7 @@ export class StasticsController {
     summary: '나이에 따른 사용자 조회 API',
     description: '특정 나이의 사용자의 수를 조회합니다.',
   })
+  @Rules('admin')
   async findByAge(@Param('age') age: number) {
     return await this.stasticsService.findByAge(age);
   }
@@ -41,6 +43,7 @@ export class StasticsController {
     summary: '최근 접속 시간에 따른 사용자 조회 API',
     description: '최근 접속 사용자의 수를 조회합니다.',
   })
+  @Rules('admin')
   async findByConnected(@Param('hour') hour: number) {
     return await this.stasticsService.findByConnected(hour);
   }
